48809b4aadc6631d3e0148c138f7bb11fc217d7d
darrenb3 <43651060+darrenb3@users.noreply.github.com>
2024-04-23 21:04:21 -0500

    update io utils to just io

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -64,7 +64,7 @@ main:
 				os.Exit(1)
 			}
 
-			responseData, err := ioutil.ReadAll(response.Body)
+			responseData, err := io.ReadAll(response.Body)
 			if err != nil {
 				log.Fatal(err)
 			}
